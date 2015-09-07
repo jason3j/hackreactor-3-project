c0c0078685fef2618be14a60918beb4cc8b860b0
Admin <JMac@Chases-MacBook-Pro.local>
2015-09-07 00:05:48 -0600

    added sudo code

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
-/**
- * Created by JMac on 9/6/15.
- */
+
+// This file is used for heroku
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+// This console.log here to test to make sure the at least part of the
+// file was loaded and can later be checked in the logs
 console.log("hey");
-app.use(express.static(__dirname + '/public'));
-
-//app.get('/', function (req, res) {
-//    res.send('Hello World!');
-//});
+app.use(express.static('public'));
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
-
-//var server = app.listen(3000, function () {
-//    var host = server.address().address;
-//    var port = server.address().port;
-//
-//    console.log('Example app listening at http://%s:%s', host, port);
-//});
--- public/charts/charts.js
+++ public/charts/charts.js
@@ -1,3 +1,6 @@
+
+//Defined variables
+
 var genOnePokemonNames = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise",
     "Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate",
     "Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran-f","Nidorina","Nidoqueen",
@@ -16,6 +19,7 @@ var genOnePokemonNames = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmel
 var poke1;
 var poke2;
 
+// The following populates the drop down list for the first pokémon list
 var sel1 = document.getElementById('dropdownList1');
 var fragment1 = document.createDocumentFragment();
 genOnePokemonNames.forEach(function(genOneNames) {
@@ -23,12 +27,11 @@ genOnePokemonNames.forEach(function(genOneNames) {
     opt.innerHTML = genOneNames;
     opt.value = genOneNames.toLocaleLowerCase();
     opt.id = "theyPickOne";
-
     fragment1.appendChild(opt);
-
 });
 sel1.appendChild(fragment1);
 
+// The following populates the drop down list for the second pokémon list
 var sel2 = document.getElementById('dropdownList2');
 var fragment2 = document.createDocumentFragment();
 genOnePokemonNames.forEach(function(genOneNames) {
@@ -37,24 +40,32 @@ genOnePokemonNames.forEach(function(genOneNames) {
     opt.value = genOneNames.toLocaleLowerCase();
     opt.id = "theyPickTwo";
     fragment2.appendChild(opt);
-
 });
+sel2.appendChild(fragment2);
 
+// This disables the second dropdown list button
 $("#dropdownMenu2").prop("disabled", true);
+// This disables the compare button
 $("#compareBtn").prop("disabled", true);
 
-sel2.appendChild(fragment2);
+// jquery
+// this function is used to call the pokémon api for the first dropdown list
 $("#dropdownList1").click(function(){
+    // This gets the lowercase name of the chosen pokémon and adds it to the url
     var text = $(event.target).text();
     document.getElementById("chosenOne").innerHTML = text;
     var result1 = document.getElementById("chosenOne").textContent.toLocaleLowerCase();
     var theUrlFirst = "https://phalt-pokeapi.p.mashape.com/pokemon/" + result1 + "/";
     var theKey = "ZpMtsjR98WmshEfAm8ywcmq5o4Osp1QtMcxjsn70UJR79HgQtO";
+    // This enables the second dropdown list button
     $("#dropdownMenu2").prop("disabled", false);
+    // Here is the ajax call for the api
     $.ajax({
         url: theUrlFirst,
         headers: {"X-Mashape-Key": theKey},
         success: function (input) {
+            // On success of the ajax call, a picture of the pokemon is shown
+            // and all the data of the first picked pokémon is saved into a variable
             document.getElementById("pokemonPictureOne").innerHTML = "";
             var picNumber = input["pkdx_id"];
             var pictureOne = document.createElement("img");
@@ -64,33 +75,43 @@ $("#dropdownList1").click(function(){
         }
     });
 });
+
+// jquery
+// this function is used to call the pokémon api for the second dropdown list
 $("#dropdownList2").click(function(){
+    // This gets the lowercase name of the chosen pokémon and adds it to the url
     var textTwo = $(event.target).text();
     document.getElementById("chosenTwo").innerHTML = textTwo;
     var result2 = document.getElementById("chosenTwo").textContent.toLocaleLowerCase();
     var theUrlSecond = "https://phalt-pokeapi.p.mashape.com/pokemon/" + result2 + "/";
     var theKey = "ZpMtsjR98WmshEfAm8ywcmq5o4Osp1QtMcxjsn70UJR79HgQtO";
-
+    // Here is the ajax call for the api
     $.ajax({
         url: theUrlSecond,
         headers: {"X-Mashape-Key": theKey},
         success: function (input) {
+            // On success of the ajax call, a picture of the pokemon is shown
+            // and all the data of the second picked pokémon is saved into a variable
             document.getElementById("pokemonPictureTwo").innerHTML = "";
             var picNumber = input["pkdx_id"];
             var pictureTwo = document.createElement("img");
             pictureTwo.src = "../Pictures/PokemonPics/" + picNumber + ".png";
             document.getElementById("pokemonPictureTwo").appendChild(pictureTwo);
             poke2 = input;
+            // The compare button is enabled once that api returns with data
             $("#compareBtn").prop("disabled", false);
-
         }
     });
 });
 
+// jquery
+// This function is called when the compare button is clicked
 $("#compareBtn").click(function () {
     combinedGraphs(poke1, poke2);
 });
 
+// The following is the code for the highcharts graphs that are
+// used once the pokémon have been picked
 function combinedGraphs (pokemonOne, pokemonTwo) {
     document.getElementById("pokemonChartCombined").innerHTML = "";
 
